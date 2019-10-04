a03449af76ddeb642175b70c8ff61fbc8644a624
John Rojcewicz <rojcewiczj@gmail.com>
2019-10-04 10:41:04 -0500

    getting started

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors')
+const projectRouter = require('./projects/projectRouter');
+const server = express();
+server.use(express.json());
+server.use(cors());
+server.use(logger)
+server.get('/', (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`)
+});
+server.use('/api/users', projectRouter);
+//custom middleware
+
+  function logger(req, res, next) {
+    console.log(
+      `[${new Date().toISOString()}] ${req.method} to ${req.url} `
+    );
+  
+    next();
+  };
+  server.listen(4000, () => {
+    console.log('\n*** Server Running on http://localhost:4000 ***\n');
+  });
+  
+
+module.exports = server;
